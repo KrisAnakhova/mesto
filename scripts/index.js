8e1d7e986f75c75fa7921ae4bb0a60cc84569152
Kristina Anakhova <kri-anakhova@yandex.ru>
2023-01-08 14:57:59 +0300

    08/01/2023 14.57

--- scripts/index.js
+++ scripts/index.js
@@ -3,12 +3,12 @@
 //кнопка редоктирования профиля
 const editButton = document.querySelector('.profile__edit-button');
 
-const addButton = document.querySelector('.profile__add-button');
+
 //нашли попап
 const popup = document.querySelector('.popup');
 //кнопка закрытия
 const closeButton = document.querySelector('.popup__close');
-//кнопка сохранить
+//кнопка сохранить / создать
 //const saveButton = document.querySelector('.popup__save');
 //нашли имя профиля
 const profTitle = document.querySelector('.profile__title');
@@ -16,13 +16,12 @@ const profTitle = document.querySelector('.profile__title');
 const profSubtitle = document.querySelector('.profile__subtitle');
 const popupOpen = document.querySelector('.popup__opened');
 
-//обьявили формы для заполнения в попап
+//обьявили формы для заполнения в попап редак.профиля
 let formPopup = document.querySelector('.popup__editform');
 let nameInput = document.querySelector('.popup__field_type_name');
 let jobInput = document.querySelector('.popup__field_type_job');
 
-
-
+//------------------------------------------------------------------------
 //открытие попап
 function openPopup(popup){
   popup.classList.add('popup_opened');
@@ -61,3 +60,26 @@ function handleFormSubmit(evt) {
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»/ сохранить
 formPopup.addEventListener('submit', handleFormSubmit);
+//-------------------------------------------------------------------------------------
+//обьявили кноопку открыть попап карточки
+const addButton = document.querySelector('.profile__add-button');
+
+//кнопка закрытия попап карточек
+const closeButtonadd = document.querySelector('.popup__close_card');
+
+//нашли попап карточек
+const popupCards = document.querySelector('.popup_cards');
+//обьявили формы для заполнения в попап карточек
+let mestoInput = document.querySelector('.popup__field_type_mesto');
+let linkInput = document.querySelector('.popup__field_type_link');
+
+//открытие попап с карточками при нажатии на кнопку
+addButton.addEventListener('click', function(){
+  openPopup(popupCards);
+});
+
+//закрытие попап при нажатии на крестик
+closeButtonadd.addEventListener('click', function(){
+  closePopup(popupCards);
+})
+
